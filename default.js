1e3d2adbc7985f61647358fdd08c96a3883e34e6
tamjidaWeb <tamjidajyoti97@gmail.com>
2024-09-08 09:45:15 +0600

    commited

--- default.js
+++ default.js
@@ -1,8 +1,8 @@
-function add(num1, num2){
+function add(num1=99, num2=0){
     const result = num1+ num2;
     // console.log(num1,num2,result);
     return result;
 }
 
-const sum = add(2,5);
-console.log(sum)
+const sum = add();
+console.log(sum);
