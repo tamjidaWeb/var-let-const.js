e925c5ce7cf55c89cab7313f152d8eaaac4c2c85
tamjidaWeb <tamjidajyoti97@gmail.com>
2024-09-08 13:16:50 +0600

    commited

--- more-arrow.js
+++ more-arrow.js
@@ -1,18 +1,63 @@
+// const difference = (x,y) => x-y;
+// const multiply = (first, second, third) => first* second* third;
+// //single or one parameter
+// const getAge = (person) => person.age;
+// const student = {name:'ananta', age:45}
+// const age = getAge(student)
+// console.log(age);
+
+// const getThird = numbers => numbers[2];
+// const third = getThird([5,3,100,5,3]);
+// console.log(third);
+
+// const doubleIt = num => num*2;
+
+// //no parameter
+
+// const getPI = ()=> Math.PI;
+// console.log(getPI());
+
+
+// //large arrow function
+
+// const doMath = (x,y,z) => {
+//     const sum = x+y+z;
+//     const mul = x*y*z;
+//     const result = sum +mul;
+
+//     return result;
+// }
+
+
+
 const difference = (x,y) => x-y;
-const multiply = (first, second, third) => first* second* third;
-//single or one parameter
-const getAge = (person) => person.age;
-const student = {name:'ananta', age:45}
-const age = getAge(student)
-console.log(age);
+const multiply = (a,b,c) => a*b*c;
+const getAge = (person)=>person.age;
+const student = {name: 'ananta', age:26};
+const age = getAge(student);
+console.log(age)
 
 const getThird = numbers => numbers[2];
-const third = getThird([5,3,100,5,3]);
+const third = getThird([4,5,3,2]);
 console.log(third);
+
+// const getThird = numbers => numbers[3];
+// const num = getThird([2,4,1,5]);
 
 const doubleIt = num => num*2;
 
 //no parameter
 
-const getPI = ()=> Math.PI;
+const getPI = () => Math.PI;
 console.log(getPI());
+
+//large arrow function
+
+const doMath = (x,y,z)=>{
+    const sum = x+y+z;
+    const sub = x-y-z;
+    return sum ;
+}
+// const 
+const summ = doMath(4,2,1);
+console.log(summ)
--- spread.js
+++ spread.js
@@ -1,21 +1,56 @@
-const max = Math.max(4,6,34,87,24,10,91,43);
-const numbers = [2,6,1,8,4,9,34,27,95];
-const arrayMax = Math.max(...numbers);
-// console.log(...numbers)
+// const max = Math.max(4,6,34,87,24,10,91,43);
+// const numbers = [2,6,1,8,4,9,34,27,95];
+// const arrayMax = Math.max(...numbers);
+// // console.log(...numbers)
 
-console.log(arrayMax);
+// console.log(arrayMax);
 
-//use spread operateor to copy
-const nums = [2,5,1,7];
-const num2 = nums;
+// //use spread operateor to copy
+// const nums = [2,5,1,7];
+// const num2 = nums;
 
-const dosto = [...nums]
+// const dosto = [...nums]
+// console.log(dosto);
+// nums.push(100);
+// console.log(dosto);
+// console.log(nums);
+
+
+// // advanced
+// const sonkha = [...nums,9999];
+// console.log(sonkha)
+
+
+const max = Math.max(5,6,2,8,9,1);
+const num = [3,5,7,1,8,9,1];
+const arrayMax = Math.max(...num);
+// console.log(...num);
+// console.log(arrayMax);
+
+//use spread operator to copy
+const friends = [3,5,1];
+const bondhu = friends;
+bondhu.push(10);
+console.log(bondhu)
+console.log(friends)
+
+
+const dosto = [...friends]//copy
 console.log(dosto);
-nums.push(100);
-console.log(dosto);
-console.log(nums);
+friends.push(100);
+// console.log(dosto);
+// console.log(friends)
 
 
-// advanced
-const sonkha = [...nums,9999];
-console.log(sonkha)
+//advanced
+
+const songkha = [...friends, 9999];//add extra element while copy
+console.log(songkha)
+
+
+
+
+
+
+
+
