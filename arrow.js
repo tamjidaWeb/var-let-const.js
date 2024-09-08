184eae12c0af59deb438e91b9354a80edf9f7dff
tamjidaWeb <tamjidajyoti97@gmail.com>
2024-09-08 10:02:12 +0600

    commited

--- arrow.js
+++ arrow.js
@@ -1,25 +1,28 @@
 //function declaration
 
-function add(a,b){
-    const result = a+b;
-    return result;
-
+function add (a,b){
+    // const result = a+b;
+    return a+b;
 }
 
 //function expression
-
 const add2 = function(a,b){
     return a+b;
 }
 
 //arrow function
-const add4 = (num1, num2, num3,num4) => num1+ num2+ num3+ num4;
-const sum4= add4(2,3,4,5);
-console.log(sum4);
 
-const multiply = (a,b) => a*b;
-const mul = multiply(2,3);
-console.log(mul);
+// const add3 = (a,b) => a+b;
+// const add = (a,b) => a*b;
+
 const add3 = (a,b) => a+b;
-const sum = add3(2,5);
-console.log(sum);
+const add4 = (num1, num2)=> num1*num2;
+const multiply = (num1,num2)=> num1*num2;
+const mult = multiply(2,7);
+console.log(mult)
+const sums = add4(2,3);
+console.log(sums)
+const sum = add3(6,7);
+console.log(sum)
+
+
--- template.js
+++ template.js
@@ -16,3 +16,11 @@ const email = `john cena
 cena lage. tmi 
 mara mari korba naki`
 console.log(email)
+
+const challenge = ``
+
+
+const numbers = [45,76,23];
+const student = {name: 'himi', age:82}
+const maths = `the sum of ${numbers[0]} and ${student.age} is ${numbers[0]+student.age}`;
+console.log(maths)
